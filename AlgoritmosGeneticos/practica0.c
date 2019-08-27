7ac56354929fd7e037643a514fd17ce07bb4d465
Emiliano Pérez <35306690+Emiyelbarto@users.noreply.github.com>
2019-08-27 13:34:31 -0500

    Cambios de codigo
    Se limpió el codigo, se añadieron valores de aleatoriedad, documentación del código al 20%

--- AlgoritmosGeneticos/practica0.c
+++ AlgoritmosGeneticos/practica0.c
@@ -8,17 +8,16 @@ Grupo: 3CM8
 #include <stdio.h>
 #include <stdlib.h>
 
-int ConseguirLongitudLista(char * Lista);
-char * PoblarLista(int longitud, char * Lista);
-void MostrarLista(char * Lista);
-char * AcomodarLista(char * Lista);
-void ListaBinario(char * Lista);
-void NumBinario(int num);
-
+int ConseguirLongitudLista(char * L);           //Función para conseguir la longitud y crear la lista
+char * PoblarLista(int l, char * L);            //Función para poblar aleatoriamente la lista
+void MostrarLista(char * L);                    //Función para mostrar la Lista
+char * AcomodarLista(char * L);                 //Función para acomodar la Lista
+void ListaBinario(char * L);                    //Función para imprimir la lista en binario
+void NumBinario(int n);                         //Función para imprimir un numero entero en binario (16 bits)
 
 int main(int argc, char const *argv[])
 {
-    int l, n;
+    int l;
     char *L;
 
     printf("Programa para generar una lista de numeros aleatorios.\n\n");
@@ -35,86 +34,83 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-int ConseguirLongitudLista(char * Lista)
+int ConseguirLongitudLista(char * L)                //Función para conseguir la longitud de una lista
 {
-    int longitud = 0;
-    while (Lista[longitud] != '\0')
-    {
-        longitud++;
-    }
-    return longitud;
+    int l;
+    l = 0;
+    while (L[l] != '\0')
+        l++;
+    return l;
 }
 
-char * PoblarLista(int longitud, char * Lista)
+char * PoblarLista(int l, char * L)                 //Función para poblar la lista con numeros aleatorios
 {
-    int n, i;
+    int i, n, fin, inicio;
     time_t t;
     srand((unsigned) time(&t));
-
-    for (i = 0; i <= longitud; i++)
+    printf("Escribir el inicio del rango de numeros aleatorios\n");
+    scanf("%d",&inicio);
+    printf("Escribir el fin del rango de numeros aleatorios:\n");
+    scanf("%d",&fin);
+    for (i = 0; i <= l; i++)
     {
-        if(i == longitud)
+        if(i == l)
         {
-            Lista[i] = '\0';
+            L[i] = '\0';
         }
         else
         {
-            n = rand() % 100 + 1;
-            Lista[i] = n;
+            n = rand() % (fin-inicio) + inicio;
+            L[i] = n;
         }
     }
-    return Lista;
+    return L;
 }
 
-void MostrarLista(char * Lista)
+void MostrarLista(char * L)
 {
-    int longitud = ConseguirLongitudLista(Lista);
+    int l = ConseguirLongitudLista(L);
     int i;
     printf("[");
-    for (i = 0; i < longitud; i++)
+    for (i = 0; i < l; i++)
     {
-        if(i != longitud-1)
-        {
-            printf("%d,",Lista[i]);
-        }
+        if(i != l-1)
+            printf("%d,",L[i]);
         else
-        {
-            printf("%d]\n",Lista[i]);
-        }
-    }
-    
+            printf("%d]\n",L[i]);
+    }    
 }
 
-char * AcomodarLista(char * Lista)
+char * AcomodarLista(char * L)
 {
     int i, j, aux;
-    int longitud = ConseguirLongitudLista(Lista);
+    int l = ConseguirLongitudLista(L);
     printf("Acomodando Lista.\n");
-    for(i = 0; i < longitud; i++)
+    for(i = 0; i < l; i++)
     {
-        for(j = i + 1; j < longitud; j++)
+        for(j = i + 1; j < l; j++)
         {
-            if(Lista[i] >= Lista[j])
+            if(L[i] >= L[j])
             {
-                aux = Lista[i];
-                Lista[i] = Lista[j];
-                Lista[j] = aux;
+                aux = L[i];
+                L[i] = L[j];
+                L[j] = aux;
             }
         }
     }
-    MostrarLista(Lista);
-    return Lista;
+    MostrarLista(L);
+    return L;
 }
 
-void ListaBinario(char * Lista)
+void ListaBinario(char * L)
 {
-    int i, longitud;
-    longitud = ConseguirLongitudLista(Lista);
-
-    for (i = 0; i < longitud; i++)
+    int i, l;
+    l = ConseguirLongitudLista(L);
+    printf("Index\tNumeroEntero\tNumeroBinario\n");
+    for (i = 0; i < l; i++)
     {
-        printf("Lista[%d]=%d=",i,Lista[i]);
-        NumBinario(Lista[i]);
+        printf("%d.- \t%d\t\t",i,L[i]);
+        NumBinario(L[i]);
     }
 }
 
@@ -129,6 +125,5 @@ void NumBinario(int num)
         else
             printf("0");
     }
- 
   printf("\n");
 }
