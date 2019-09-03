c8e762e6c9dbe1af3321fc755da58a86a2b9e284
emiliano <emiliano@localhost.localdomain>
2019-09-03 12:44:25 -0500

    Se añadio función cuadrada pero falta histograma

--- AlgoritmosGeneticos/Practica1/main.c
+++ AlgoritmosGeneticos/Practica1/main.c
@@ -13,16 +13,18 @@ int * CrearLista();
 int * PoblarLista(int * L);
 void MostrarLista(int * L);
 void MostrarHistograma(int *L);
+int * FuncionCuadrado(int * L);
 
 int main(int argc, char const *argv[])
 {
-    printf("Programa para mostrar una lista de 10 numeros aleatorios junto a su histograma\n\n");
+    printf("\n\nPrograma para mostrar una lista de 10 numeros aleatorios junto a su histograma\n\n");
     srand(time(NULL));
     int * Lista;
     Lista = CrearLista();
     Lista = PoblarLista(Lista);
     printf("Lista de Numeros Aleatorios -> ");
     MostrarLista(Lista);
+    Lista = FuncionCuadrado(Lista);
     printf("\nHistograma: \n\n");
     MostrarHistograma(Lista);
     return 0;
@@ -40,7 +42,7 @@ int * PoblarLista(int * L)
     srand(time(NULL));
     int i;
     for (i = 0; i < 10; i++)
-        L[i] = rand() % 100;
+        L[i] = rand() % 10;
     return L;
 }
 
@@ -78,15 +80,25 @@ void MostrarHistograma(int *L)
         printf("\n");
     }
     printf("\n");
-    for ( i = 0; i < 200; i++)
+}
+
+//funcion para evaluar numeros enteros f(x) = x²
+
+int * FuncionCuadrado(int * L)
+{
+    int i;
+    for (i = 0; i < 10; i++)
     {
-        if (i == 199)
-        {
-            printf("-\n");
-        }
+        L[i] = L[i] * L[i];
+    }
+    
+    printf("\nLista de Numeros x² -> A²[10]={");
+    for(i = 0; i < 10; i++)
+    {
+        if (i == 9)
+            printf("%d}\n",L[i]);
         else
-        {
-            printf("-");
-        }        
-    }  
+            printf("%d,",L[i]);
+    }
+    return L;
 }
