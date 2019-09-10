f352dffbdd4e9e0b7e6e6e7118869f1d39158014
emiyelbarto <emiyelbarto@hotmail.com>
2019-09-10 12:00:42 -0500

    Terminado Oficialmente

--- AlgoritmosGeneticos/Practica1/main.c
+++ AlgoritmosGeneticos/Practica1/main.c
@@ -7,17 +7,11 @@ Grupo: 3CM8
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-double * CrearLista();
-double * PoblarLista(int n, double * L);
-double * FuncionCuadrado(double * L);
-int LongitudLista(double * L);
-void MostrarLista(double * L);
-void MostrarHistograma(double *L);
-int NumeroMaximo(int l, double *L);
+#include "practica0.h"
 
 int main(int argc, char const *argv[])
 {
+    Practica0();
     printf("\n\nPrograma para mostrar una lista de 10 a N numeros aleatorios junto a su histograma\n");
     double * Lista;
     Lista = CrearLista(Lista);
@@ -103,7 +97,7 @@ void MostrarHistograma(double *L)
 
         for (i = L[ubicacion]; i > 0; --i) {
         printf("%.3d|", i);
-        for (j = 0; j < 10; ++j)
+        for (j = 0; j < l; ++j)
             (L[j] >= i) ? printf(" X ") : printf("   ");
         printf("\n");
     }
@@ -130,4 +124,115 @@ int NumeroMaximo(int l, double *L)
         }
     }
     return ubicacion;
+}
+
+
+int ConseguirLongitudLista(char * L)                //Función para conseguir la longitud de una lista
+{
+    int l;
+    l = 0;
+    while (L[l] != '\0')
+        l++;
+    return l;
+}
+
+char * PoblarListaPractica0(int l, char * L)                 //Función para poblar la lista con numeros aleatorios
+{
+    int i, n, fin, inicio;
+    time_t t;
+    srand((unsigned) time(&t));
+    printf("Escribir el inicio del rango de numeros aleatorios\n");
+    scanf("%d",&inicio);
+    printf("Escribir el fin del rango de numeros aleatorios:\n");
+    scanf("%d",&fin);
+    for (i = 0; i <= l; i++)
+    {
+        if(i == l)
+        {
+            L[i] = '\0';
+        }
+        else
+        {
+            n = rand() % (fin-inicio) + inicio;
+            L[i] = n;
+        }
+    }
+    return L;
+}
+
+void MostrarListaPractica0(char * L)
+{
+    int l = ConseguirLongitudLista(L);
+    int i;
+    printf("[");
+    for (i = 0; i < l; i++)
+    {
+        if(i != l-1)
+            printf("%d,",L[i]);
+        else
+            printf("%d]\n",L[i]);
+    }    
+}
+
+char * AcomodarLista(char * L)
+{
+    int i, j, aux;
+    int l = ConseguirLongitudLista(L);
+    printf("Acomodando Lista.\n");
+    for(i = 0; i < l; i++)
+    {
+        for(j = i + 1; j < l; j++)
+        {
+            if(L[i] >= L[j])
+            {
+                aux = L[i];
+                L[i] = L[j];
+                L[j] = aux;
+            }
+        }
+    }
+    MostrarListaPractica0(L);
+    return L;
+}
+
+void ListaBinario(char * L)
+{
+    int i, l;
+    l = ConseguirLongitudLista(L);
+    printf("Index\tNumeroEntero\tNumeroBinario\n");
+    for (i = 0; i < l; i++)
+    {
+        printf("%d.- \t%d\t\t",i,L[i]);
+        NumBinario(L[i]);
+    }
+}
+
+void NumBinario(int num)
+{
+    int i, j;
+    for (i = 15; i >= 0; i--)
+    {
+        j = num >> i;
+        if (j & 1)
+            printf("1");
+        else
+            printf("0");
+    }
+  printf("\n");
+}
+
+void Practica0()
+{
+    int lBin;
+    char *LBin;
+    printf("Programa para generar una lista de numeros aleatorios.\n\n");
+    printf("Escribir cuantos numeros quiere que tenga la lista:\n");
+    scanf("%d",&lBin);
+    printf("Creando Lista de %d numeros.\n",lBin);
+    LBin = calloc(sizeof(int) * (lBin+1),0);
+    LBin = PoblarListaPractica0(lBin,LBin);
+    MostrarListaPractica0(LBin);
+    LBin = AcomodarLista(LBin);
+    ListaBinario(LBin);
+    getchar();
 }
