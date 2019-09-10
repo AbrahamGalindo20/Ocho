f4fb5cdd32a56df1e5d26c2144b069d5c9cade2e
Emiliano Pérez <35306690+Emiyelbarto@users.noreply.github.com>
2019-09-10 11:37:30 -0500

    Practica2 Terminada
    Se añadio la función de imprimir el histograma, se corrigieron bugs y se añadieron dos códigos para referencias futuras.

--- AlgoritmosGeneticos/Practica1/main.c
+++ AlgoritmosGeneticos/Practica1/main.c
@@ -4,101 +4,130 @@ Autor: José Emiliano Pérez Garduño
 Materia: Genetic Algorithms
 Grupo: 3CM8
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int * CrearLista();
-int * PoblarLista(int * L);
-void MostrarLista(int * L);
-void MostrarHistograma(int *L);
-int * FuncionCuadrado(int * L);
+double * CrearLista();
+double * PoblarLista(int n, double * L);
+double * FuncionCuadrado(double * L);
+int LongitudLista(double * L);
+void MostrarLista(double * L);
+void MostrarHistograma(double *L);
+int NumeroMaximo(int l, double *L);
 
 int main(int argc, char const *argv[])
 {
-    printf("\n\nPrograma para mostrar una lista de 10 numeros aleatorios junto a su histograma\n\n");
-    srand(time(NULL));
-    int * Lista;
-    Lista = CrearLista();
-    Lista = PoblarLista(Lista);
-    printf("Lista de Numeros Aleatorios -> ");
-    MostrarLista(Lista);
+    printf("\n\nPrograma para mostrar una lista de 10 a N numeros aleatorios junto a su histograma\n");
+    double * Lista;
+    Lista = CrearLista(Lista);
     Lista = FuncionCuadrado(Lista);
-    printf("\nHistograma: \n\n");
     MostrarHistograma(Lista);
     return 0;
 }
 
-int * CrearLista()
+int LongitudLista(double * L)
 {
-    int *L;
-    L = malloc(sizeof(int) * 10);
+    int i = 0;
+    while(L[i] != 0)
+        i++;
+    return i;
+}
+
+double * CrearLista(double *L)
+{
+    int n;
+    printf("Introducir cuantos numeros a partir de 10 tendra la lista (10 + n)\n");
+    scanf("%d",&n);
+    printf("Se creara una lista de tamano: 10 + %d = %d\n", n, 10+n);
+    L = calloc(sizeof(double) * (10 + n),0);
+    L = PoblarLista(n,L);
     return L;
 }
 
-int * PoblarLista(int * L)
+double * PoblarLista(int n, double * L)
 {
     srand(time(NULL));
     int i;
-    for (i = 0; i < 10; i++)
-        L[i] = rand() % 10;
+    for (i = 0; i < 10+n; i++)
+        L[i] = rand() % 100 + 1;
+    MostrarLista(L);
     return L;
 }
 
-void MostrarLista(int *L)
+void MostrarLista(double *L)
 {
+    printf("Lista de Numeros Aleatorios -> ");
     int i;
-    printf("A[10]={");
-    for(i = 0; i < 10; i++)
+    int l = LongitudLista(L);
+    printf("A[%d]={",l);
+    for(i = 0; i < l; i++)
     {
-        if (i == 9)
-            printf("%d}\n",L[i]);
+        if (i == l-1)
+            printf("%.0lf}\n",L[i]);
         else
-            printf("%d,",L[i]);
+            printf("%.0lf,",L[i]);
     }
 }
 
-void MostrarHistograma(int *L)
-{
-    int i,j;
-    for (i = 0; i < 10; i++)
-    {
-        if(i == 9)
-        {
-            printf("[%d]",i);
-            for(j = 0; j < L[i]; j++)
-                printf("+");
-            
-        }
-        else
-        {
-            printf("[%d]",i);
-            for(j = 0; j < L[i]; j++)
-                printf("+");
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
-//funcion para evaluar numeros enteros f(x) = x²
-
-int * FuncionCuadrado(int * L)
+double * FuncionCuadrado(double * L)                  //funcion para evaluar numeros enteros f(x) = x²
 {
     int i;
-    for (i = 0; i < 10; i++)
+    int l = LongitudLista(L);
+    for (i = 0; i < l; i++)
     {
         L[i] = L[i] * L[i];
     }
     
-    printf("\nLista de Numeros x² -> A²[10]={");
-    for(i = 0; i < 10; i++)
+    printf("\nLista de Numeros x² -> A²[%d]={",l);
+    for(i = 0; i < l; i++)
     {
-        if (i == 9)
-            printf("%d}\n",L[i]);
+        if (i == l-1)
+            printf("%.0lf}\n",L[i]);
         else
-            printf("%d,",L[i]);
+            printf("%.0lf,",L[i]);
     }
     return L;
+}
+
+void MostrarHistograma(double *L)
+{
+    printf("\nHistograma: \n\n");
+    int i,j,ubicacion;
+    int l = LongitudLista(L);
+    ubicacion = NumeroMaximo(l,L);
+    //Parte del eje Y
+    printf("El numero maximo es: %0.lf\n",L[ubicacion]);
+    
+    printf("--------------------------------------------------\n");
+
+        for (i = L[ubicacion]; i > 0; --i) {
+        printf("%.3d|", i);
+        for (j = 0; j < 10; ++j)
+            (L[j] >= i) ? printf(" X ") : printf("   ");
+        printf("\n");
+    }
+    printf("   ");
+
+
+    for (i = 0; i < l; ++i) //Parte del eje X
+        printf("%3d", i+1);
+    printf("\n--------------------------------------------------\n");
+}
+
+int NumeroMaximo(int l, double *L)
+{
+    double max;
+    max = L[0];
+    int i;
+    int ubicacion;
+    for (i = 0; i < l; i++)
+    {
+        if(L[i] > max)
+        {
+            max = L[i];
+            ubicacion = i;
+        }
+    }
+    return ubicacion;
 }
