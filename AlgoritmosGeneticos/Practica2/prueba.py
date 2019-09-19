883f45318c2e225143f7b6ea1d6ac58853b46561
emiyelbarto <emiyelbarto@hotmail.com>
2019-09-18 19:44:03 -0500

    Se finalizó la practica correctamente

--- AlgoritmosGeneticos/Practica2/prueba.py
+++ AlgoritmosGeneticos/Practica2/prueba.py
@@ -6,19 +6,22 @@ import re
 
 def GenerarBitAleatorio():
     x = random.random()
-    print(x)
+    #print(x)
     if x > 0.5:
-        print("El valor es mayor a .5, asi que es 1")
+        #print("El valor es mayor a .5, asi que es 1")
         return 1
     else:
-        print("El valor es menor a .5, asi que es 0")
+        #print("El valor es menor a .5, asi que es 0")
         return 0
 
 def GenerarNumeroAleatorio(rango):
     num = []
+    aux = '';
     for x in range(0,rango):
-        print("de {} a rango {}".format(x,rango))
-        num.append(GenerarBitAleatorio())
+        #print("de {} a rango {}".format(x,rango))
+        aux = aux + str(GenerarBitAleatorio())
+        #num.append(GenerarBitAleatorio())
+    num = int(aux)
     return num
 
 def EsEntero(valor):
@@ -28,6 +31,21 @@ def EsEntero(valor):
         return True
     else:
         return False
+    
+def VerificarInputNumListas():
+    flag = False
+    while not flag:
+        num = raw_input("Introducir un numero par de arreglos para crear\n")
+        flag = EsEntero(num)
+        if (flag == False):
+            print("Introducir un numero entero")
+        else:
+            num = int(num)
+            if ((num % 2) == 0):
+                return num
+            else:
+                print("Introducir un numero par")
+                flag = False
 
 def VerificarInputTamano(i):
     flag = False
@@ -61,23 +79,18 @@ def VerificarInputRango(i):
     return rango
 
 def CrearLista(i):
-    #longitud = VerificarInputTamano(i)
-    #rango = VerificarInputRango(i)
-    longitud = 6
-    rango = 6
+    longitud = VerificarInputTamano(i)
+    rango = VerificarInputRango(i)
     Lista = []
     for x in range (0 , longitud):
         a = GenerarNumeroAleatorio(rango)
-        #print("Anexando el valor " + str(a) + " a la lista en la posicion " + str(x+1))
         Lista.append(a)
     return Lista
 
-def PoblarLista(L):
+def PoblarLista(L,x):
     aux = []
-    for x in range (0,4):
-        aux = CrearLista(x)
-        print("La lista " + str(x+1) + " queda como: " + str(aux))
-        L.append(aux)
+    aux = CrearLista(x)
+    L.append(aux)
     return L
 
 def ImprimirLista(L):
@@ -90,51 +103,48 @@ def ImprimirLista(L):
                 print(L[x][y]),
                 print(","),
         print("]\n")
-        
-def ImprimirListaBinaria(L):
-    for x in range (0,4):
-        print("Lista[{}] = [".format(x+1)),
-        for y in range (0, len(L[x])):
-            if (y == len(L[x]) - 1) :
-                print(L[x][y]),
-            else:
-                print(L[x][y]),
-                print(","),
-        print("]\n")
 
 def FuncionBinario(L):
-    aux = L
-    for y in range (0 , 4):
+    for y in range (0 , len(L)):
         for x in range (0 , len(L[y])):
-            #aux[y][x] = bin(L[y][x])[2:].zfill(8)
-            aux[y][x] = bin(L[y][x])[2:]
-    return aux
+            L[y][x] = int(str(L[y][x]),2)
+    return L
 
 def FuncionGray(L):
     aux = L
-    for y in range(0,4):
+    for y in range(0,len(L)):
         for x in range(0,len(L[y])):
-            aux[y][x] = int(L[y][x], 2)
             aux[y][x] ^= (aux[y][x] >> 1)
-            aux[y][x] = bin(aux[y][x])[2:]
-            #aux[y][x] = bin(aux[y][x])[2:].zfill(8)
+            aux[y][x] = bin(int(aux[y][x]))[2:]
     return aux
-        
+
+
+
+def CrearArreglos(L):
+    num = VerificarInputNumListas()
+    for x in range(0,num):
+        L = PoblarLista(L,x)
+    return L
+    
 
 def main():
-    print("Practica 2")
-    Arreglos = []
-    Arreglos = PoblarLista(Arreglos)
-    ArreglosBinario = []
-    ArreglosGray = []
-    print("Todas las listas quedan como: ")
-    ImprimirLista(Arreglos)
-    print("Todas las listas en binario quedan como:")
-    ArreglosBinario = FuncionBinario(Arreglos)
-    ImprimirListaBinaria(ArreglosBinario)
-    print("Todas las listas en codigo gray quedan como:")
-    ArreglosGray = FuncionGray(Arreglos)
-    ImprimirListaBinaria(ArreglosGray)
+    print("Jose Emiliano Perez Garduno")
+    print("********Practica 2********")
+    Decimales = []
+    Binarios = [[010,010,0110,0100],[101,101,1001,1011],[010,010,0110,0100],[101,101,1001,1011]]
+    Gray = []
+    '''
+    Decimales = CrearArreglos(Decimales)
+    Decimales = PoblarLista(Binarios,4)
+    print("Todas las listas quedan como: \n")
+    ImprimirLista(Binarios)
+    print("Todas las listas en decimal quedan como:\n")
+    Decimales = FuncionBinario(Binarios)
+    ImprimirLista(Decimales)
+    '''
+    ArreglosGray = FuncionGray(Binarios)
+    print("\nTodas las listas en codigo gray quedan como:\n")
+    ImprimirLista(ArreglosGray)
     
 if __name__ == '__main__':
     main()
