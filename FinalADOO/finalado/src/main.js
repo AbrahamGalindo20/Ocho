ba06f884c097755beb0d12aee6d931169acbf14e
Emiliano <emiyelbarto@hotmail.com>
2019-11-27 22:08:03 -0600

    Stream dinamico funcional.

--- FinalADOO/finalado/src/main.js
+++ FinalADOO/finalado/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
+import LoadScript from "vue-plugin-load-script";
 
 Vue.config.productionTip = false;
 
@@ -10,3 +11,5 @@ new Vue({
     router,
     render: h => h(App),
 }).$mount("#app");
+
+Vue.use(LoadScript);
--- FinalADOO/finalado/src/views/Streamings.vue
+++ FinalADOO/finalado/src/views/Streamings.vue
@@ -1,57 +1,90 @@
 <template>
-  <div class="streamings">
-    <v-app>
-      <v-card class="mx-auto mt-2" max-width="auto" tile id="videos_list">
-        <v-list-item v-for="video in videos" :key="video">
-          <v-list-item-content>
-            <v-list-item-title>{{ video.title }}</v-list-item-title>
-            <v-list-item-subtitle>{{ video.text }}</v-list-item-subtitle>
-            <v-list-item-subtitle>{{ video.url }}</v-list-item-subtitle>
-            <iframe
-              src="https://player.twitch.tv/?video=v367046564&autoplay=true"
-              frameborder="0"
-            ></iframe>
-          </v-list-item-content>
-        </v-list-item>
+  <div class="streamingsView">
+    <v-app id="reproductor">
+      <v-card height="100%">
+        <VueTwitchPlayer :channel="datos[number].channel"></VueTwitchPlayer>
       </v-card>
     </v-app>
   </div>
 </template>
 
 <script>
+import VueTwitchPlayer from "vue-twitch-player";
+
 export default {
+  components: {
+    VueTwitchPlayer
+  },
   data() {
     return {
-      videos: [
+      datos: [
         {
-          title: "vid1",
-          text: "text1",
-          url: "https://player.twitch.tv/?video=v367046564&autoplay=true"
+          title: "RiotGames",
+          channel: "riotgames",
+          descripcion: "Canal oficial de Riotgames"
         },
         {
-          title: "vid2",
-          text: "text2",
-          url: "https://player.twitch.tv/?video=v367046564&autoplay=true"
+          title: "OverwatchLeague",
+          channel: "overwatchleague",
+          descripcion: "Canal oficial de Overwatch"
         },
         {
-          title: "vid3",
-          text: "text3",
-          url: "https://player.twitch.tv/?video=v367046564&autoplay=true"
+          title: "PokémonLeague",
+          channel: "pokemon",
+          descripcion: "Canal oficial de Pokémon"
         },
         {
-          title: "vid4",
-          text: "text4",
-          url: "https://player.twitch.tv/?video=v367046564&autoplay=true"
+          title: "CSGO League",
+          channel: "esl_csgo",
+          descripcion: "Canal oficial de Counter Strike"
         },
         {
-          title: "vid5",
-          text: "text5",
-          url: "https://player.twitch.tv/?video=v367046564&autoplay=true"
+          title: "The international",
+          channel: "dota2ti",
+          descripcion: "Canal oficial de Dota 2"
+        },
+        {
+          title: "Fortnite",
+          channel: "fortnite",
+          descripcion: "Canal oficial de Fortnite"
         }
-      ]
+      ],
+      min: 0,
+      max: 5,
+      number: 3
     };
+  },
+  created: function() {
+    this.getRandomNumber();
+  },
+  methods: {
+    loadNum: function() {
+      this.min = 1;
+      this.max = 10;
+      this.getRandomNumber();
+    },
+    getInput: function() {
+      let min = Number(this.min);
+      let max = Number(this.max);
+      if (min > max) {
+        [min, max] = [max, min];
+      }
+      this.min = min;
+      this.max = max;
+      this.getRandomNumber();
+    },
+    getRandomNumber: function() {
+      this.number = this.generateNumber();
+    },
+    generateNumber: function() {
+      return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+    }
   }
 };
 </script>
 
-<style></style>
+<style lang="scss">
+#reproductor {
+  height: 100%;
+}
+</style>
